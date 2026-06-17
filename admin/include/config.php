0e183a587123c1b5205b05b19bd9be4361fab5a5
Shwe Yee <shweyeewinlae26@gmail.com>
2026-06-17 09:37:46 +0630

    Update database connection settings

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,6 +1,6 @@
 <?php
-    define('DB_HOST','localhost');
+    define('DB_HOST','db');
     define('DB_USER','root');
-    define('DB_PASS','');
+    define('DB_PASS','root_password');
     define('DB_NAME','dbwedding');
 ?>
